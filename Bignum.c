6132489eda1fa7bc9291cea0cdb9d5a68a166176
YeongHun Kim <kyh941031@naver.com>
2016-10-29 16:58:22 +0900

    Create Bignum.c

---
+++ Bignum.c
@@ -0,0 +1,81 @@
+#include "Bignum.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+Bigint * BIn(char * snum)
+{
+	Bigint *bnum = (Bigint *)malloc(sizeof(Bigint));
+	char *snum_t;  // 십진수를 역순으로 저장
+	char *bnum_t;  // 이진수를 역순으로 저장
+	int slen, blen, blen_t, biglen;
+	int i, j;
+	int n;  // snum의 실시간 최대길이
+	int temp;
+
+	if (snum[0] == '-' || snum[0] == '+')  // 부호가 붙음
+		slen = strlen(snum) - 1;
+	else                                   // 부호 안붙음
+		slen = strlen(snum);
+	snum_t = (char *)malloc((slen+1) * sizeof(char));
+	n = slen;
+	
+	blen_t = (int)floor(slen / log10(2.0)) + 1 + 16;   // blen, slen에 관한 특수한 부등식 사용
+	bnum_t = (char *)malloc(blen_t * sizeof(char));
+
+	// snum을 idx[1]부터 역순으로 저장 //
+	snum_t[0] = 0;
+	if (snum[0] == '-' || snum[0] == '+')  // 부호가 붙음
+		for (i=1; i<=slen; i++)
+			snum_t[i] = snum[slen-i+1] - '0';
+	else                                   // 부호 안붙음
+		for (i=1; i<=slen; i++)
+			snum_t[i] = snum[slen-i] - '0';
+
+	// 10진수 -> 2진수 변환 //
+	blen = 0;
+	while (n > 0)
+	{
+		snum_t[0] = 0;
+		for (i=0; i<=n-1; i++)
+		{
+			snum_t[i] = (snum_t[i+1] % 2) * 5 + snum_t[i];
+			snum_t[i+1] = snum_t[i+1] / 2;
+		}
+		bnum_t[blen++] = snum_t[0] & 1;
+		n = n - !(snum_t[n]);
+	}
+	while (blen%16 != 0)
+		bnum_t[blen++] = 0;   // 
+
+	// 2진수를 bnum에 저장 //
+	biglen = (int)ceil(blen / 16.0);
+	bnum->len = biglen;
+	bnum->num = (unsigned int *)calloc(biglen, sizeof(unsigned int));
+	
+	for (i=0; i<biglen; i++)
+	{
+		for (j=0, temp=0; j<16; j++)
+			temp += bnum_t[16*i+j] << j;
+		bnum->num[i] = temp;
+	}
+
+	if (snum[0] == '-')
+		bnum->sign = NEG;
+	else if (snum[0] == '+')
+		bnum->sign = POS;
+	else      // 부호 안붙음
+		bnum->sign = POS;
+
+	// 동적 메모리 해제 //
+	free(snum_t);
+	free(bnum_t);
+	
+	return bnum;
+}
+
+
+char * BOut(Bigint *bnum)
+{
+}
