4686eb7bec260d9b93cfbd2414802e5984224f03
YeongHun Kim <kyh941031@naver.com>
2016-11-15 21:09:58 +0900

    Modify everything

--- BignumOp.c
+++ BignumOp.c
@@ -28,6 +28,8 @@ void BcarryIn(Bigint *B)
 		B->len = n+1;
 }
 
+
+
 // 큰 두 양수의 덧셈 //
 Bigint * BAdd(Bigint *result, Bigint *B1, Bigint *B2)
 {
@@ -61,12 +63,10 @@ Bigint * BAdd(Bigint *result, Bigint *B1, Bigint *B2)
 			n[i] = n1[i];
 	}
 	
-	// 초기화된 값이 있으면 덮어씌움 //
+	// 초기화된 값이 있으면 덮어씌움 (기존값 제거) //
 	if (result->num != NULL)
-	{
 		free(result->num);
-		result->num = n;
-	}
+	result->num = n;
 	result->len = (flag)? B2->len : B1->len;
 	result->sign = POS;
 	BcarryIn(result);  // 자리올림
@@ -83,11 +83,19 @@ int BCompare(Bigint *B1, Bigint *B2)
 	int len;
 	int i;
 
+	// 두 수에 0이 포함될 때
+	if (s1 == ZRO && s2 == ZRO)
+		return ZRO;
+	else if (s1 == ZRO)
+		return -s2;
+	else if (s2 == ZRO)
+		return s1;
+
 	// 두 수의 부호가 다를때
 	if (s1*s2 == NEG)
 	{
-		if (s1 == NEG) return -1;
-		else           return 1;
+		if (s1 == NEG) return NEG;
+		else           return POS;
 	}
 	
 	// 두 수의 부호가 같을때
@@ -103,18 +111,18 @@ int BCompare(Bigint *B1, Bigint *B2)
 			break;
 
 	if (i < 0)
-		return 0;   // 같음
+		return ZRO;   // 같음
 	else if (p1[i] > p2[i])
 		return s1;  // (+):B1>B2, (-):B1<B2
 	else // (p1[i] < p2[i])
 		return -s1; // (+):B1>B2, (-):B1<B2
 }
 
-// 미완성: 큰 두 양수의 뺄셈 //
+// 큰 두 양수의 뺄셈 //
 Bigint * BSub(Bigint *result, Bigint *B1, Bigint *B2)
 {
 	unsigned int *n, *nb, *ns;
-	int lenb, lens;
+	int lenb, lens, lenr;
 	int i;
 	
 	int flag;
@@ -122,26 +130,58 @@ Bigint * BSub(Bigint *result, Bigint *B1, Bigint *B2)
 	// 두 수의 크기 비교
 	flag = BCompare(B1, B2);
 
-	if (flag == 1) {
+	if (flag == 1) {  // 결과가 양수
 		nb = B1->num, ns = B2->num;
 		lenb = B1->len, lens = B2->len;
-	} else if (flag == -1) {
+	} else if (flag == -1) {  // 결과가 음수
 		nb = B2->num, ns = B1->num;
 		lenb = B2->len, lens = B1->len;
+	} else {     // 결과가 0 (같음)
+		// 초기화된 값이 있으면 덮어씌움 //
+		if (result->num != NULL)
+			free(result->num);
+		result->num = (unsigned int *)calloc(1, sizeof(unsigned int));
+		result->len = 1;
+		result->sign = ZRO;
+		return result;
 	}
-	
+
 	n = (unsigned int *)calloc(lenb, sizeof(unsigned int));
 
 	// 뺄셈 //
-	n[0] = nb[0] - ns[0] + 0x0000FFFF;         // 큰수-작은수+carry(받음)
+	n[0] = nb[0] - ns[0] + ((lenb <= 1)? 0 : 0x00010000);     // 큰수-작은수+carry(받음)
 
-	for (i=0; i<lens; i++)
-		n[i] = nb[i] - ns[i] + 0x0000FFFF - 1; // 큰수-작은수+carry(받음)-carry(줌)
+	for (i=1; i<lens; i++)
+		n[i] = nb[i] - ns[i] + 0x00010000 - 1; // 큰수-작은수+carry(받음)-carry(줌)
 	
 	for (i=lens; i<lenb-1; i++)
-		n[i] = nb[i] + 0x0000FFFF - 1;         // 큰수+carry(받음)-carry(줌)
+		n[i] = nb[i] + 0x00010000 - 1;         // 큰수+carry(받음)-carry(줌)
 
-	n[lenb-1] = nb[lenb-1] - 1;        // 큰수-carry(줌)
+	if (lenb > lens)
+		n[lenb-1] = nb[lenb-1] - 1;            // 큰수-carry(줌)
 
+	// carry in 수행(각 자리마다 최대 한 자리 carry 가능) //
+	for (i=0; i<lenb; i++)
+	{
+		n[i+1] += n[i] >> 16;
+		n[i] = n[i] & 0x0000FFFF;
+	}
+	
+	// 수의 길이 조정 //
+	lenr = lenb;
+	for (i=lenb-1; i>=1; i--)
+	{
+		if (n[i] != 0)
+			break;
+		lenr--;
+	}
 
+	if (result->num != NULL)
+		free(result->num);
+	result->num = n;
+	result->len = lenr;
+	result->sign = flag;
+
+	return result;
 }
+
